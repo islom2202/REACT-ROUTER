215329b227a8dc373d81ab5bbd5fa71fa1f9ccef
islom2202 <islomihsan@gmail.com>
2023-11-29 11:36:11 +0500

    test

--- src/Pages/Pagination.jsx
+++ src/Pages/Pagination.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useSearchParams, NavLink } from 'react-router-dom'
 export const Pagination = () => {
-  // const API = "https://jsonplaceholder.typicode.com/posts"
+  const API = "https://jsonplaceholder.typicode.com/posts"
 
   const [posts, setPosts] = useState([])
   // Fetch Data
@@ -50,7 +50,6 @@ export const Pagination = () => {
           </button>
         ))}
       </ul>
-      <ul></ul>
     </div>
   )
 }
